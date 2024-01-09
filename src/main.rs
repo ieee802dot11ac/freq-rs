e3a1cdb8953e710df66c3b69be0483116ea9bfbc
First Last <sillybegani@gmail.com>
2024-01-08 17:36:48 -0800

    :(

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs};
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
@@ -75,13 +75,62 @@ impl RndFile {
             println!("New entry of type {} named {} with unk {}", entry.filetype, entry.filename, entry.unk);
             self.entries.push(entry);
         }
-        let _ = src.read(self.files.as_mut_slice())?;
+        let x = src.read_to_end(&mut self.files)?;
+        if x == 0 {
+            println!("no files?");
+        }
         Ok(())
     }
 
-    pub fn export(&mut self, dump: &mut PathBuf) -> Result<(), Box<dyn Error>> {
-        let mut sliced_filestack = self.files.as_mut_slice();
-
+    pub fn export(&mut self, dump: &PathBuf) -> Result<(), Box<dyn Error>> {
+        let sliced_filestack = self.files.as_mut_slice();
+        let mut files_windows = sliced_filestack.windows(4);
+        let mut offsets = vec![0usize];
+        let mut files_vecs: Vec<Vec<u8>> = vec![vec![]];
+        for i in 0..self.entry_ct {
+            match files_windows.position(|delim| delim == [0xADu8, 0xDE, 0xAD, 0xDE]) {
+                Some(x) => {
+                    offsets.push(x); 
+                    println!("new idiot found sized {x}");
+                    files_vecs.push(vec![0; x]);
+                    let mut f = dump.clone();
+                    f.push(&self.entries[i as usize].filename);
+                    fs::write(f, files_vecs[i as usize].as_slice())?;
+                },
+                None => continue
+            }
+        }
+         // let mut files_2 = sliced_filestack.split(|delim| delim == [0xADu8, 0xDE, 0xAD, 0xDE]);
+        /*let mut files_2: Vec<Vec<u8>> = vec![vec![0u8]];
+        let mut parts_of_delim_found = 0;
+        let mut file_idx = 0;
+        for byt in sliced_filestack {
+            match byt {
+                0xAD => if parts_of_delim_found == 0 || parts_of_delim_found == 2 {
+                    parts_of_delim_found += 1;
+                } else {
+                    files_2[file_idx].push(*byt);
+                    parts_of_delim_found = 0;
+                },
+                0xDE => if parts_of_delim_found == 3 {
+                    parts_of_delim_found = 0;
+                    file_idx += 1;
+                    println!("found file #{file_idx}");
+                } else if parts_of_delim_found == 1 {
+                    parts_of_delim_found += 1;
+                } else {
+                    files_2[file_idx].push(*byt);
+                    parts_of_delim_found = 0;
+                },
+                _ => files_2[file_idx].push(*byt),
+            }
+        }
+        if file_idx == 0 {
+            panic!("what (zero files?)")
+        }
+        if self.entry_ct != (file_idx - 1).try_into().unwrap() {
+            panic!("FUCK (entry count didn't match file count");
+        }*/
         Ok(())
     }
 }
@@ -89,8 +138,8 @@ impl RndFile {
 #[derive(clap::Parser)]
 struct Args {
     input_rnd: PathBuf,
-    #[arg(short, required = false)]
-    output_dir: PathBuf,
+    #[arg(short)]
+    output_dir: Option<PathBuf>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -103,6 +152,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     } else if in_ext == "rnd" {
         let mut input_file = File::open(&args.input_rnd)?;
         let _ = rnd.load(&mut input_file)?;
+        let mut outdir;
+        match args.output_dir.clone() {
+            Some(_) => outdir = args.output_dir.unwrap(),
+            None => {
+                outdir = PathBuf::new();
+                outdir.push(args.input_rnd.parent().unwrap().to_path_buf());
+                outdir.push("_".to_owned() + args.input_rnd.file_stem().unwrap().to_str().unwrap())
+            }
+        }
+        if !outdir.is_dir() {
+            fs::create_dir(&outdir)?;
+        }
+        rnd.export(&outdir)?;
     }
 
     Ok(())
